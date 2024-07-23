b8a182ec1775a4fc32ad2fc4e18af4449fb7e8db
HsiaoHungWang <wang@ispan.com.tw>
2024-07-23 19:36:14 +0800

    urls.py 參數的使用

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,9 @@ from myapp import views
 urlpatterns = [
     # path('比對的路徑','要執行的function')
     # http://127.0.0.1:8000/store/
-    path('', views.index)
+    path('', views.index),
+    # http://127.0.0.1:8000/store/about/
+    path('about/', views.about),
+    path('about/<int:year>', views.about)
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 # Create your views here.
 def index(request):
     return HttpResponse('<h2>myapp 首頁</h2>')
+
+def about(request, year=datetime.now().year):
+    return HttpResponse(f'<h2>About { year }</h2>')
