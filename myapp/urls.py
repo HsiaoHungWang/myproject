ea25107976e086f1e178ce13ba61897d5b91a255
HsiaoHungWang <wang@ispan.com.tw>
2024-07-23 20:22:01 +0800

    參數的使用

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,8 +5,15 @@ urlpatterns = [
     # path('比對的路徑','要執行的function')
     # http://127.0.0.1:8000/store/
     path('', views.index),
+
+    # http://127.0.0.1:8000/store/details/uuid
+    path('details/<uuid:product_id>', views.details),
     # http://127.0.0.1:8000/store/about/
     path('about/', views.about),
-    path('about/<int:year>', views.about)
+     # http://127.0.0.1:8000/store/about/2000
+    path('about/<int:year>', views.about),
+     # http://127.0.0.1:8000/store/blog/2024/7/1
+    path('blog/<path:publish>', views.blog),
+    path('course/<slug:course_name>', views.course)
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,22 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>myapp 首頁</h2>')
+    return HttpResponse('<h2>讀出所有商品資料</h2>')
 
+def details(reqeust, product_id=''):
+    return HttpResponse(f'<h2>讀出商品編號{ product_id }的商品</h2>')
+
+
+# http://127.0.0.1:8000/store/about/2000
+#  2000 會傳給 year 這個參數
 def about(request, year=datetime.now().year):
     return HttpResponse(f'<h2>About { year }</h2>')
+
+# publish => 2024/7/23
+def blog(request, publish=None):
+     return HttpResponse(f'<h2>讀取 { publish } 的文章</h2>')
+
+# course_name => python-web-framework
+def course(request, course_name=None):
+    return HttpResponse(f'<h2>課程名稱：{ course_name }</h2>')
+    
