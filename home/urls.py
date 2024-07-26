3864f5652d33452e8e0ba7f30d50b849f8bcff07
HsiaoHungWang <wang@ispan.com.tw>
2024-07-26 21:40:05 +0800

    url tag 的使用

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from home import views
 
+app_name='home'
 urlpatterns = [
     # http://127.0.0.1:8000/
-    path('', views.index),
+    path('', views.index, name='index'),
     path('countries/', views.countries),  # http://127.0.0.1:8000/countries/
     path('about/', views.about),  # http://127.0.0.1:8000/about/
     path('about/<int:year>', views.about),  # http://127.0.0.1:8000/about/2000
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
 from myapp import views
 
+app_name='myapp'
 urlpatterns = [
     # path('比對的路徑','要執行的function')
     # http://127.0.0.1:8000/store/
-    path('', views.index),
+    path('', views.index, name='index'),
 
     # http://127.0.0.1:8000/store/details/uuid
     path('details/<uuid:product_id>', views.details),
     # http://127.0.0.1:8000/store/about/
-    path('about/', views.about),
+    path('about/', views.about, name='about'),
      # http://127.0.0.1:8000/store/about/2000
     path('about/<int:year>', views.about),
      # http://127.0.0.1:8000/store/blog/2024/7/1
     path('blog/<path:publish>', views.blog),
     path('course/<slug:course_name>', views.course),
      # http://127.0.0.1:8000/store/show  
-    path('show/', views.show)
+    path('show/', views.show, name='show')
 ]
