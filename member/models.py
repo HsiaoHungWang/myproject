16b48f07b053b4d0f36c05dbfdc7ebb50775d7cc
HsiaoHungWang <wang@ispan.com.tw>
2024-08-01 20:41:09 +0800

    CodeFirst

--- member/models.py
+++ member/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Member(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    member_name = models.CharField(max_length=20, unique=True)
+    member_password = models.CharField(max_length=128)
+    member_birth = models.DateField()
+    member_address = models.CharField(max_length=200, null=True)
+    last_update = models.DateTimeField(default=datetime.now())
+
+    class Meta:
+        db_table = 'member'  # 指定的資料表名稱
+
