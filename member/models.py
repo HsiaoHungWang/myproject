f839c2259582383300c6b1899aaa02df377b75bf
HsiaoHungWang <wang@ispan.com.tw>
2024-08-06 19:20:25 +0800

    member 資料表中新增一個欄位

--- member/models.py
+++ member/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+
 # Create your models here.
 class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
@@ -7,6 +8,7 @@ class Member(models.Model):
     member_password = models.CharField(max_length=128)
     member_birth = models.DateField()
     member_email = models.EmailField(max_length=200, unique=True, default='')
+    member_avatar = models.CharField(max_length=100, default='empty.png')
     last_update = models.DateTimeField(default=datetime.now())
 
     class Meta:
